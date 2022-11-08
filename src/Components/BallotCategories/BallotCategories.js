c85b3194472e09b361c865a558ef21245066b5ae
Michael Chilaka <obichilaka@gmail.com>
2022-11-08 08:03:22 +0100

    completed BallotCategory component

---
+++ src/Components/BallotCategories/BallotCategories.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Nominee from '../Nominee/Nominee';
+
+import "./ballotCategories.css";
+
+const BallotCategories = ({ ballot, moveSelections }) => {
+    const [selection, setSelection] = useState('');
+
+    const showNominees = (nominees) => {
+        return nominees.map(nominee => {
+            const highlight = nominee.id === selection ? "highlight" : ""
+            return <Nominee
+                key={nominee.id}
+                id={nominee.id}
+                title={nominee.title}
+                image={nominee.imageUrl}
+                highlight={highlight}
+            />
+        })
+    }
+
+    const toggleHighlight = (e) => {
+        if (e.target.id) {
+            setSelection(e.target.id)
+            moveSelections(e.target.id, ballot.id)
+        }
+    }
+
+    return (
+        <>
+            <div className='categories'>
+                <h2 className='categories__header'>{ballot.title}</h2>
+            </div>
+            <div className='categories__main' onClick={e => toggleHighlight(e)}>
+                {showNominees(ballot.items)}
+            </div>
+        </>
+    )
+}
+
+export default BallotCategories;
